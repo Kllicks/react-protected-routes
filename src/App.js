d51511f0d14853af3a22976771289c4bbc35b90e
Kyle Sekellick <sekellickkyle@yahoo.com>
2019-01-13 23:58:35 -0500

    adds dom dependency

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Link, Redirect, withRouter } from "react-router-dom";
 
 class App extends Component {
+
+
 	render() {
 	return (
 		<div className="App">
-			
+
 		</div>
 	);
 	}
